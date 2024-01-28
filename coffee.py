130583a9496c08ff2966b5fd0f313b9088737b2a
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-28 23:56:48 +0000

    add documentation for data structure in the coffee shop program

--- coffee.py
+++ coffee.py
@@ -1,5 +1,38 @@
-#ClydeBank Coffee Shop Simulator 4000
+# ClydeBank Coffee Shop Simulator 4000
+# Copyright 2022 (C) ClydeBank Media, All Rights Reserved.
 
+# Current day number
+day = 1
+
+# Sales list of dictionaries
+# sales = [
+#     {
+#         "day": 1,
+#         "coffee_inv": 100,
+#         "advertising": "10",
+#         "temp": 68,
+#         "cups_sold": 16
+#     },
+#     {
+#         "day": 2,
+#         "coffee_inv": 84,
+#         "advertising": "15",
+#         "temp": 72,
+#         "cups_sold": 20
+#     },
+#     {
+#         "day": 3,
+#         "coffee_inv": 64,
+#         "advertising": "5",
+#         "temp": 78,
+#         "cups_sold": 10
+#     },
+# ]
+
+# Create an empty sales list
+sales = []
+
+# Print welcome message
 print("ClydeBank Coffee Shop Simulator 4000, Version 1.00")
 print("Copyright (C) 2023 ClydeBank Media, All Rights Reserved,\n")
 print("Let's collect some information before we start the game.\n")
