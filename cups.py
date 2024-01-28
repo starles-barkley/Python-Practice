53444ddc96322756b2990852af95b02484039ff2
starles-barkley <144153159+starles-barkley@users.noreply.github.com>
2024-01-28 21:43:02 +0000

    create cups.py

---
+++ cups.py
@@ -0,0 +1,4 @@
+price = input("What is the price of a cup of coffee? ")
+cups = input("How many cups do you want? ")
+total = float(price) * int(cups)
+print("Your total is $" + str(total) + " for " + cups + " cups.")
